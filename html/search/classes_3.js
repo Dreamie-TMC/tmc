90aa25dd4b1686b13e68241acd3b92bac98097cf
notyourav <notyourav@users.noreply.github.com>
2022-08-04 18:45:53 +0000

    deploy: bbcea04f9ef17002bb98f1793ff058dc34b4f235

--- html/search/classes_3.js
+++ html/search/classes_3.js
@@ -1,16 +1,17 @@
 var searchData=
 [
   ['deathfxobject_0',['DeathFxObject',['../structDeathFxObject.html',1,'']]],
-  ['destructibletileobservemanager_1',['DestructibleTileObserveManager',['../structDestructibleTileObserveManager.html',1,'']]],
-  ['destructibletileobservemanager_5funk_2',['DestructibleTileObserveManager_unk',['../structDestructibleTileObserveManager__unk.html',1,'']]],
-  ['dialog_3',['Dialog',['../structDialog.html',1,'']]],
-  ['diggingcaveentrance_4',['DiggingCaveEntrance',['../structDiggingCaveEntrance.html',1,'']]],
-  ['diggingcaveentrancemanager_5',['DiggingCaveEntranceManager',['../structDiggingCaveEntranceManager.html',1,'']]],
-  ['diggingcaveentrancetransition_6',['DiggingCaveEntranceTransition',['../structDiggingCaveEntranceTransition.html',1,'']]],
-  ['dirtparticleentity_7',['DirtParticleEntity',['../structDirtParticleEntity.html',1,'']]],
-  ['dmaregisters_8',['DMARegisters',['../structDMARegisters.html',1,'']]],
-  ['doublebookshelfentity_9',['DoubleBookshelfEntity',['../structDoubleBookshelfEntity.html',1,'']]],
-  ['droptable_10',['Droptable',['../unionDroptable.html',1,'']]],
-  ['dungeonlayout_11',['DungeonLayout',['../structDungeonLayout.html',1,'']]],
-  ['dustentity_12',['DustEntity',['../structDustEntity.html',1,'']]]
+  ['delayedentityloadmanager_1',['DelayedEntityLoadManager',['../structDelayedEntityLoadManager.html',1,'']]],
+  ['destructibletileobservemanager_2',['DestructibleTileObserveManager',['../structDestructibleTileObserveManager.html',1,'']]],
+  ['destructibletileobservemanager_5funk_3',['DestructibleTileObserveManager_unk',['../structDestructibleTileObserveManager__unk.html',1,'']]],
+  ['dialog_4',['Dialog',['../structDialog.html',1,'']]],
+  ['diggingcaveentrance_5',['DiggingCaveEntrance',['../structDiggingCaveEntrance.html',1,'']]],
+  ['diggingcaveentrancemanager_6',['DiggingCaveEntranceManager',['../structDiggingCaveEntranceManager.html',1,'']]],
+  ['diggingcaveentrancetransition_7',['DiggingCaveEntranceTransition',['../structDiggingCaveEntranceTransition.html',1,'']]],
+  ['dirtparticleentity_8',['DirtParticleEntity',['../structDirtParticleEntity.html',1,'']]],
+  ['dmaregisters_9',['DMARegisters',['../structDMARegisters.html',1,'']]],
+  ['doublebookshelfentity_10',['DoubleBookshelfEntity',['../structDoubleBookshelfEntity.html',1,'']]],
+  ['droptable_11',['Droptable',['../unionDroptable.html',1,'']]],
+  ['dungeonlayout_12',['DungeonLayout',['../structDungeonLayout.html',1,'']]],
+  ['dustentity_13',['DustEntity',['../structDustEntity.html',1,'']]]
 ];
